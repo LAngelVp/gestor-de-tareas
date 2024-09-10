83e8350d4cb37390c59f5a7aa4e69639c31eae86
Luis Angel <neal.ocaffry@gmail.com>
2024-09-10 11:54:32 -0600

    cambios

--- src/app/components/lista-tareas/lista-tareas.component.ts
+++ src/app/components/lista-tareas/lista-tareas.component.ts
@@ -17,20 +17,52 @@ import { FormsModule } from '@angular/forms';
 export class ListaTareasComponent implements OnInit {
   lista_tareas: TareaModel[] = [];
   visible : boolean = false;
+  tareas_filtradas : TareaModel[] = [];
+  estado_filtro : string = 'Todo';
 
   constructor(private local_storage: ServicioLocalStorageService){}
 
   ngOnInit(): void {
-    this.lista_tareas = this.local_storage.mostrar_elementos()
+    this.lista_tareas = this.local_storage.mostrar_elementos();
+    this.filtrar_tareas();
   }
 
   eliminar_elemento(posicion: number): void{
     this.local_storage.eliminar_elemento(posicion);
     this.lista_tareas = this.local_storage.mostrar_elementos();
+    this.filtrar_tareas();
   }
 
   actualizar_esado_tarea(tarea: TareaModel):void {
     this.local_storage.actualizar_elemento(tarea);
     this.lista_tareas = this.local_storage.mostrar_elementos();
+    this.filtrar_tareas();
+  }
+
+  filtrar_tareas(): void {
+    switch (this.estado_filtro) {
+      case 'Completada':
+        this.tareas_filtradas = this.lista_tareas.filter(tarea => tarea.estado === 'Completada');
+        break;
+      case 'En Proceso':
+        this.tareas_filtradas = this.lista_tareas.filter(tarea => tarea.estado === 'En Proceso');
+        break;
+      case 'Sin Iniciar':
+        this.tareas_filtradas = this.lista_tareas.filter(tarea => tarea.estado === 'Sin Iniciar');
+        break;
+      default:
+        this.tareas_filtradas = this.lista_tareas;
+        break;
+    }
+  }
+
+  cambiarFiltro(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    this.estado_filtro = selectElement.value;
+    this.filtrar_tareas();
+  }
+
+  get totalTareasFiltradas(): number {
+    return this.tareas_filtradas.length;
   }
 }
